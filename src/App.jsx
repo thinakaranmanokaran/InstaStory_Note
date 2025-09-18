5944ee949153848f51efa601fcc7d8ea77fe4e0c
thinakaranmanokaran <thinakaranmanohar@yahoo.com>
2025-09-18 10:18:49 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import MobileView from './components/MobileView'
+import { div } from 'framer-motion/client'
 
 const App = () => {
     return (
-        <div className='md:hidden font-dmsans'><MobileView /></div>    
+        <div className='font-dmsans'>
+            <div className='md:hidden '><MobileView /></div>
+            <div className='text-center text-6xl font-bold tracking-tighter md:flex h-screen items-center justify-center hidden '>Currently Only for Mobile View</div>
+        </div>
     )
-}   
+}
 
 export default App
